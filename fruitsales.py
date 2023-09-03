da82db5dfaa574b1b36eaf49711be16ebe9d8dd3
mingeea1 <mingeea1@gmail.com>
2023-09-03 16:16:17 -0400

    Add my code

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,14 @@
 # add your code here
+import pandas as pd
+
+# Create a dictionary with the data
+data = {
+    'Apples': [35, 41],
+    'Bananas': [21, 34]
+}
+
+# Create a DataFrame from the dictionary
+df = pd.DataFrame(data, index=['2017 Sales', '2018 Sales'])
+
+# Print the DataFrame
+print(df)
